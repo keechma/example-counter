fc612029ef5a1c3bd378cf1fd7dc54c92d541fba
Mihael Konjevic <konjevic@gmail.com>
2016-02-10 14:54:57 +0100

    Add documentation for the counter controller

--- src/keechma_counter/counter_controller.cljs
+++ src/keechma_counter/counter_controller.cljs
@@ -3,11 +3,17 @@
             [cljs.core.async :refer [<!]])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defrecord Controller []
+(defrecord ^{:doc
+"Implements the counter controller.
+
+- `params` function returns `true` because this controller should be always running.
+- `start` function sets the default counter value (0). `:kv` is used to store any key - value pairs
+- `handler` function is waiting for commands on the `in-chan`. When the command comes (if it's `:inc` or `:dec`) it will increment or decrement the counter."
+}
+  Controller []
   controller/IController
   (params [_ _] true)
-  (start
-    [_ params app-db]
+  (start [_ params app-db]
     (assoc-in app-db [:kv :counter] 0))
   (handler [_ app-db-atom in-chan _] 
     (go (loop []
